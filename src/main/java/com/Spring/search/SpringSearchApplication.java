ced56926805b79d04adb76a92200711c30575eb6
irmantasdv <irmantas.dv@gmail.com>
2020-04-09 18:44:20 +0300

    first commit

---
+++ src/main/java/com/Spring/search/SpringSearchApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSearchApplication.class, args);
+	}
+
+}
